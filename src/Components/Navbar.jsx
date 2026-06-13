f973b54dede05c0144f04b956f71813211e9f9f7
Aladin <myxcomputer92@gmail>
2026-06-13 23:52:52 +0500

    Hero section is completed but not responsive yet

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -4,7 +4,11 @@ const Navbar = () => {
   return (
     <div className='w-full h-[12vh] bg-[#FFD900]'>
       <nav className='p-8 flex justify-between'>
-        <h1 className='text-xl bg-black text-white h-[5vh] pt-1 w-[14vw] text-center uppercase font-first border-2 border-transparent rounded-sm cursor-pointer'>Mamooth Murals</h1>
+        <h1 className='text-xl bg-black text-white h-[5vh] pt-1 w-[14vw] text-center uppercase font-first border-2 border-transparent rounded-sm cursor-pointer'>
+          <a href="http://localhost:5173">
+          Mamooth Murals
+          </a>
+          </h1>
         <ul  className='flex gap-0'>
             <li className='text-[0.4cm] font-medium uppercase cursor-pointer text-center pt-2 w-[6vw] h-[5.5vh] border-t-2 border-l-2 border-r-2 border-black rounded-md mr-5 transition-all hover:bg-black hover:text-white hover:duration-75'>Home</li>
             <li className='text-[0.4cm] font-medium uppercase cursor-pointer text-center pt-2 w-[6vw] h-[5.5vh] border-t-2 border-l-2 border-r-2 border-black rounded-md mr-5 transition-all hover:bg-black hover:text-white hover:duration-75'>About</li>
